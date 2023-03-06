7957c627e9c307e277e35ec57ab298994c126ce2
Kévin BUANNEC <noxtariadev@gmail.com>
2023-03-06 17:19:35 +0100

    Ajout d'un bouton error 404

--- src/components/routes/ErrorPage.jsx
+++ src/components/routes/ErrorPage.jsx
@@ -1,4 +1,5 @@
 import { useRouteError } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const ErrorPage = () => {
   const error = useRouteError()
@@ -7,6 +8,13 @@ const ErrorPage = () => {
     <div className="d-flex flex-column justify-content-center align-items-center" style={{height: "100vh", width: "100vw"}}>
       <h3>Error {error.status}!</h3>
       <p>{error.data}</p>
+
+      <Link
+                      to={`/`}
+                      className="nav-link btn btn-info p-2 rounded"
+                    >
+                    Retour à l'accueil
+                    </Link>
     </div>
   )
 }
